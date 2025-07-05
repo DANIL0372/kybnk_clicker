62cfb553afa49c9d1d6840a531a96386e6214daf
DANISIMO <68996918+DANIL0372@users.noreply.github.com>
2025-07-05 14:16:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,8 @@ def serve_spa(path):
 # Точка входа для Vercel
 def handler(request):
     return app(request)
+
+application = app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
