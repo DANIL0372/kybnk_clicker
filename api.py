f65c7a1c33b5f3409b97c9fa2e793e0a06b765d0
DANISIMO <68996918+DANIL0372@users.noreply.github.com>
2025-07-05 15:58:15 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory, render_template
 
-app = Flask(__name__, 
+app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 
@@ -9,12 +9,15 @@ app = Flask(__name__,
 def index():
     return render_template('index.html')
 
+# Упрощённый и корректный обработчик статики
 @app.route('/static/<path:subpath>')
 def serve_static(subpath):
-    return send_from_directory(app.static_folder, subpath)
+    return send_from_directory('static', subpath)
 
+# Обработчик для всех путей SPA
 @app.route('/<path:path>')
 def serve_spa(path):
     return render_template('index.html')
 
+# WSGI-совместимый экспорт
 application = app
