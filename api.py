f11aec3c9fd265c0216c53ab3b82e1a85fa94c4d
DANISIMO <68996918+DANIL0372@users.noreply.github.com>
2025-07-05 14:53:46 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,24 +1,21 @@
 import os
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, request
 
-app = Flask(__name__,
-            static_folder='static',
-            template_folder='templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Основной маршрут
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Маршрут для статических файлов
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+# Новый обработчик статики
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
-# Обработчик для всех остальных путей (SPA)
+# Обработчик для всех путей SPA
 @app.route('/<path:path>')
-def catch_all(path):
+def serve_spa(path):
     return render_template('index.html')
 
-# Обязательно для Vercel
+# WSGI-совместимый экспорт
 application = app
